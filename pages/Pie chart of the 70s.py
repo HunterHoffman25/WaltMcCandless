f9cccfeb4d302b75c53d21ec319d489c877529d2
HunterHoffman25 <25hhoffman23489750@gmail.com>
2024-05-17 13:07:30 -0400

    Update Pie chart of the 70s.py

--- pages/Pie chart of the 70s.py
+++ pages/Pie chart of the 70s.py
@@ -1,20 +1,32 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import streamlit as s
+import streamlit as st
 
-s.title("Top 10 Countries From Your Choice of year")
+st.title("Top 10 Countries From 70s till Now")
+
+# Use st.sidebar.selectbox if you want it in the sidebar
+year = st.selectbox("Select Year", [1970, 1980, 1990, 2000, 2010, 2020])
+
 uf = r'pages/wp.csv'
-year = s.selectbox("Select Year", [1970, 1980, 1990, 2000, 2010, 2015, 2020, 2022])
 
 if uf is not None:
-    sc = ['Country', '1970 Population']
-    df = pd.read_csv(uf, names=sc)
-    df_year = df[['Country', str(year) + ' Population']]
-    top10 = df_year.nlargest(10, str(year) + ' Population')
-
-
+    df = pd.read_csv(uf)
+    
+    # Debugging: Print out column names
+    print("Column Names:", df.columns)
+    
+    # Filter data for the selected year
+    year_column = str(year) + ' Population'
+    df_year = df[['Country', year_column]]
+    
+    # Debugging: Print out selected DataFrame
+    print("Selected DataFrame:", df_year)
+    
+    top10 = df_year.nlargest(10, year_column)
+    
+    # Plotting
     fig, ax = plt.subplots()
-    ax.pie(top10[str(year) + ' Population'], labels=top10['Country'], autopct='%1.1f%%', startangle=140)
+    ax.pie(top10[year_column], labels=top10['Country'], autopct='%1.1f%%', startangle=140)
     ax.set_title('Population Distribution in {}'.format(year))
     plt.show()
     st.pyplot(fig)
