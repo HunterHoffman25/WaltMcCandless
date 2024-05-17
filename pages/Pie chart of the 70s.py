9c8c5aa9f18084fc83c697d9035dfbe25290e8d1
HunterHoffman25 <25hhoffman23489750@gmail.com>
2024-05-17 12:52:09 -0400

    Update Pie chart of the 70s.py

--- pages/Pie chart of the 70s.py
+++ pages/Pie chart of the 70s.py
@@ -9,11 +9,12 @@ year = s.selectbox
 if uf is not None:
     sc = ['Country', year]
     df = pd.read_csv(uf, names=sc)
+    top5 = df.nsmallest(10, '1970 Population')
 
 
     df.set_index('Country', inplace=True)  
 
-    df.plot(kind='pie', y='1970 Population', figsize=(8, 8), autopct='%1.1f%%', startangle=140)
+    df.plot(kind='pie', y=top5, figsize=(8, 8), autopct='%1.1f%%', startangle=140)
     plt.title('Population Distribution in 1970')
     plt.ylabel('')  
     plt.show()
