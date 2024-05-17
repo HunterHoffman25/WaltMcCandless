ce4a17edc96f914725a53c02c7a4fc649df57319
HunterHoffman25 <25hhoffman23489750@gmail.com>
2024-05-17 13:02:38 -0400

    Update Pie chart of the 70s.py

--- pages/Pie chart of the 70s.py
+++ pages/Pie chart of the 70s.py
@@ -2,19 +2,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as s
 
-s.title("Top 10 Countries From 70s till Now")
+s.title("Top 10 Countries From Your Choice of year")
 uf = r'pages/wp.csv'
-year = s.selectbox
+year = s.selectbox("Select Year", [1970, 1980, 1990, 2000, 2010, 2015, 2020, 2022])
 
 if uf is not None:
     sc = ['Country', '1970 Population']
     df = pd.read_csv(uf, names=sc)
-    top5 = df.nsmallest(10, '1970 Population')
+    df_year = df[['Country', str(year) + ' Population']]
+    top10 = df_year.nlargest(10, str(year) + ' Population')
 
 
-    df.set_index('Country', inplace=True)  
-
-    df.plot(kind='pie', y=top5['1970 Population'], figsize=(8, 8), autopct='%1.1f%%', startangle=140)
-    plt.title('Population Distribution in 1970')
-    plt.ylabel('')  
+    fig, ax = plt.subplots()
+    ax.pie(top10[str(year) + ' Population'], labels=top10['Country'], autopct='%1.1f%%', startangle=140)
+    ax.set_title('Population Distribution in {}'.format(year))
     plt.show()
+    st.pyplot(fig)
